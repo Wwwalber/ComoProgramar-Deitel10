6d814ee2c78d1d0cbe8c97c0c0b9d923c4c3b9c3
Walber Costa <walberoc@gmail.com>
2023-06-27 04:41:45 -0300

    teste de contrutor com parâmetros

---
+++ src/cap3/Account.java
@@ -0,0 +1,18 @@
+package cap3;
+
+public class Account {
+    private String name; // variável de instância   
+
+    public Account(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
